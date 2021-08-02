63fbe03ce7bd77bb078cf5841f15ffb8a5d8edba
shob9138 <shoaibahmed9138@gmail.com>
2021-08-02 21:50:46 +0530

    feat - snap scroll

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./topbar.scss";
+
+export default function Topbar() {
+    return (
+        <div className="topbar">
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro">asdfs</a>
+                </div>
+                
+                <div className="right">
+
+                </div>
+            </div>
+        </div>
+    )
+}

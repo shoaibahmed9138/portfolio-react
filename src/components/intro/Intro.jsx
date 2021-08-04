11f308b43d029d30040b2b4882c12913c207b331
shob9138 <shoaibahmed9138@gmail.com>
2021-08-04 22:04:37 +0530

    feat - intro

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,10 +1,17 @@
 import React, {useEffect, useRef} from 'react'
 import "./intro.scss"
+import { init } from 'ityped';
+
 export default function Intro() {
     const textRef = useRef()
 
     useEffect(()=>{
-        console.log(textRef);
+        init( textRef.current, {
+            showCursor:true,
+            backDelay:1500,
+            backSpeed:60,
+            strings:["Developer", "Student", "Engineer"]
+        })
     },[])
 
     return (
@@ -18,7 +25,7 @@ export default function Intro() {
                 <div className="wrapper">
                     <h2>Hey there, I'm</h2>
                     <h1>Shoaib Ahmed</h1>
-                    <h3>Aspiring <span ref={textRef}>Developer</span></h3>
+                    <h3>An Aspiring <span ref={textRef}></span></h3>
                 </div>
                 <a href="#portfolio">
                     <img src="assets/down.png" alt="/"></img>
